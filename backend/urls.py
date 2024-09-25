c8b9e16198a25245c01ad8831aab4d70a767a088
rababk <rababkhalifamohammed@gmail.com>
2024-09-25 22:14:13 +0400

    2

--- backend/urls.py
+++ backend/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ', include('portfolioCode.urls')),
+    path('portfolio/', include('portfolioCode.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
